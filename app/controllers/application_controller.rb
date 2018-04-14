8ac1df1c52f2cb523be427401031d42a461d3ec8
Ankita George <ankita@rewind.io>
2018-04-14 17:51:27 -0400

    override sign_in path and sign out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, :city, :email, :password ])
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    home_index_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
