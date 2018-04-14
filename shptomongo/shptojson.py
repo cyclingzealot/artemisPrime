148de7c85f7c8ade08abeec413e7287813e003db
Maëlys McArdle <airseal@hotmail.com>
2018-04-14 14:05:34 -0400

    Different processing approach for GeoJSON generation

--- shptomongo/shptojson.py
+++ shptomongo/shptojson.py
@@ -1,59 +1,49 @@
-import shapefile
-import json
-import datetime
+"""
+Converts SHP file to KML.
+"""
+
 import argparse
+import subprocess
 
 def main():
-    """
-    Convert the SHP file to GeoJSON.
-    """
-    arguments = load_command_line_arguments()
+  """
+  Converts the SHP file to KML
+  """
 
-    features = load_shp_file(arguments.input)
+  arguments = load_command_line_arguments()
 
-    feature_to_geojson(features, arguments.output)
+  convert_shp_to_geojson(arguments.input, arguments.output)
+
 
 def load_command_line_arguments():
-    """
-    Load command-line arguments.
-    """
-    parser = argparse.ArgumentParser(description='Converts SHP files to GeoJSON.')
-    parser.add_argument('--input', '-i', required=True,
-                        help='the SHP file to convert')
-    parser.add_argument('--output', '-o', default='output.json',
-                        help='the GeoJSON file to output to')
-    args = parser.parse_args()
-    return args
+  """
+  Load command-line arguments.
+  """
+  parser = argparse.ArgumentParser(description='Converts SHP files to KML.')
+  parser.add_argument('--input', '-i', required=True,
+                      help='the SHP file to convert')
+  parser.add_argument('--output', '-o', default='output.geojson',
+                      help='the GeoJSON file to output to')
+  args = parser.parse_args()
+  return args
 
-def load_shp_file(input_file):
-    # read the shapefile
-    reader = shapefile.Reader(input_file)
-    fields = reader.fields[1:]
-    field_names = [field[0] for field in fields]
-    features = []
-    for sr in reader.shapeRecords():
-        atr = dict(zip(field_names, sr.record))
-        geom = sr.shape.__geo_interface__
-        features.append(dict(type="Feature", geometry=geom, properties=atr))
-    return features
 
-def feature_to_geojson(features, output_path):
-    """Converts feature to GeoJSON."""
+def convert_shp_to_geojson(input_path, output_path):
+  """
+  Converts SHP to KML
+  """
+  convert_binary = 'ogr2ogr'
 
-    # Convert to JSON.
-    json_contents = json.dumps(
-        {"type": "FeatureCollection", "features": features}, 
-        default=json_serial)
-
-    # Write the GeoJSON file.
-    with open(output_path, "w") as geojson:
-        geojson.write(json_contents)
-
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
-    if isinstance(obj, (datetime.date, datetime.datetime)):
-        return obj.isoformat()
-    raise TypeError ("Type %s not serializable" % type(obj))
+  try:
+    completed_process = subprocess.run(
+      [convert_binary,'-f','GeoJSON', output_path, input_path],
+      stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  except FileNotFoundError:
+    print("The program '%s' is missing. You need to have that installed " +
+          "in order to use this script." % convert_binary)
+  else:
+    print(completed_process.stdout.decode("utf-8"))
+    return completed_process.returncode
 
 if __name__ == "__main__":
-    main()
+  main()
