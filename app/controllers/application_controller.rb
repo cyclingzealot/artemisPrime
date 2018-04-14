a8e2e4beaa05fc800434976fe45348d1bfa52a15
Ankita George <ankita@rewind.io>
2018-04-14 18:28:52 -0400

    don't include sign out if devise method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  layout :layout_by_resource
 
   protected
 
@@ -14,5 +15,13 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
   end
 end
