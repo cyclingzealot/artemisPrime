4cc0fe5745e85adfd0a9350e9b0060c8b930833e
Ankita George <ankita@rewind.io>
2018-04-14 19:50:20 -0400

    update report back to be just notes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,13 @@ class HomeController < ApplicationController
     # polling_area = PollingArea.find_by name: params[:area]
     # polling_area = PollingArea.find 1
     # PamphletEffort.create user: current_user, pollingArea: polling_area, reportBack: params[:notes]
+
+    pamphlet_effort_id = current_user.pamphlet_effort.id
+    redirect_to home_index_path, flash: { error: "You don't have an assigned polling area to report about." } unless pamphlet_effort_id
+
+    pamphlet_effort = PamphletEffort.find(pamplet_effort_id)
+    pamphlet_effort.update_attribute(reportBack: params[:notes])
+
     redirect_to home_index_path, flash: { notice: 'Your report has been sent.' }
   end
 end
