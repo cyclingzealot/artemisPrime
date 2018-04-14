e72ba1403d2de039bb4297f2cab3414e84d2dda1
Julien Lamarche <julien.lamarche@gmail.com>
2018-04-14 19:51:47 -0400

    modified fields of pamphlet effort volunter->user, pollingAres->polingAreaId

--- app/models/pamphlet_effort.rb
+++ app/models/pamphlet_effort.rb
@@ -1,9 +1,10 @@
 class PamphletEffort
   include Mongoid::Document
   field :reportBack, type: String
+  field :pollingAreaId, type: String
 
-  belongs_to :volunteer
-  belongs_to :pollingArea
+  belongs_to :user
+
   #field :volunteer, type: Reference
   #field :pollingArea, type: Reference
   #field :state, type: Reference
