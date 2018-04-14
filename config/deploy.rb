7a6fb724403da848cbcf99db7689d236a9682baa
Julien Lamarche <julien.lamarche@gmail.com>
2018-04-14 14:22:06 -0400

    deployment configuration fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "~> 3.10.1"
 
 set :application, "artemisPrime"
-set :repo_url, "git@github.com:cyclingzealot/tlaloc.git"
+set :repo_url, "git@github.com:cyclingzealot/artemisPrime.git"
 
 # Default branch is :master
  ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/home/artemis/artemis"
+set :deploy_to, "/home/artemis/artemis"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -31,6 +31,7 @@ set :repo_url, "git@github.com:cyclingzealot/tlaloc.git"
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
+set :local_user, "artemis"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 10 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,8 @@ server "ubuntu@ec2-54-145-123-77.compute-1.amazonaws.com", user: "artemis", role
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+set :branch, "release"
+
 
 
 # role-based syntax
